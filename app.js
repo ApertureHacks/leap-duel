7a8fe2151657c50ddbb95f6a1a32de305c2fd92b
Josh Matthews <josh@jmatthews.us>
2013-09-29 00:59:36 -0400

    Starting to add base game logic with websockets.

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ if ('development' === app.get('env')) {
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+var currentUsers = 0; //FIXME: less hackish way to handle this?
+var playsers = [];
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 
@@ -40,8 +43,47 @@ server.listen(app.get('port'), function(){
 });
 
 // Websockets
+
 io.sockets.on('connection', function(socket) {
-    socket.on('woop', function(data) {
-        console.log(data);
+    socket.on('join', function(data) {
+      if (currentUsers < 2) {
+        currentUsers++;
+        var player = players[currentUsers] = {
+          id: currentUsers,
+          socket: socket,
+          state: 'ready',
+          health: 100
+        };
+
+        socket.on('request-block', function(data){
+          if (player.state === 'ready') {
+            players[1].socket.emit('block', {player: player.id});
+            players[2].socket.emit('block', {player: player.id});
+
+            player.state = 'blocking';
+            setTimeout(function(player){
+              player.state = 'ready';
+            }, 1000);
+          }
+        });
+
+        socket.on('request-attack', function(data){
+          if (player.state === 'ready') {
+            players[1].socket.emit('attack', {player: player.id});
+            players[2].socket.emit('attack', {player: player.id});
+
+            player.state = 'attacking';
+            setTimeout(function(player){
+              //FIXME: check for hit on other player
+              player.state = 'ready';
+            }, 1000);
+          }
+        });
+      }
+
+      if (currentUsers >= 2) {
+        players[1].emit('start');
+        players[2].emit('start');
+      }
     });
 });
